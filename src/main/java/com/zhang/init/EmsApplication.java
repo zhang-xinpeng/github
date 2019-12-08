c54282183f7ad86b6234e6aa53070484c562130e
zhang <1195411685@qq.com>
2019-12-08 09:40:41 +0800

    登录页面开发完成

--- src/main/java/com/zhang/init/EmsApplication.java
+++ src/main/java/com/zhang/init/EmsApplication.java
@@ -3,7 +3,6 @@ package com.zhang.init;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
